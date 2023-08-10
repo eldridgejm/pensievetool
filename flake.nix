530fcae74e503b839d54af41607aba14099bc7af
Justin Eldridge <eldridgejm@gmail.com>
2023-08-09 22:36:40 -0600

    Change nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Tool for managing a Pensieve";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
   outputs = {
     self,
